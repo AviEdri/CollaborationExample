b95041ce283ac09dff9ae304f2ada9b519f90290
DorKremer <dorkremer1597@gmail.com>
2021-03-11 15:41:03 +0200

    sum function

--- src/Man.java
+++ src/Man.java
@@ -1,15 +1,10 @@
 
 public class Man {
 	
-	private Woman wife;
-	private int age;
-	
 	public Man() {
-		this.age = 30;
 	}
 
 	public void setWife(Woman wife) {
-		this.wife = wife;
 	}
 	
 	public void doSomething() {

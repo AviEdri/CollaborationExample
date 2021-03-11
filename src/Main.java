602203da21b1e37907597a5050d3585571034371
Ariye Caspi <caspi_j@hotmail.com>
2021-03-11 15:44:33 +0200

    my commit ariye

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		//my commit pizdetz
+		//my commit pizdetzz
 
 		String tmpDirectoryName = System.getenv("TMP");
 		System.out.println(tmpDirectoryName);

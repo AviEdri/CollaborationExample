19470ba8145744b9e44f9c65cb424984cd0b2959
sasi3013 <sasi.yaron@gmail.com>
2021-03-11 15:15:36 +0200

    sum

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		String tmpDirectoryName = System.getenv("TMP");
 		System.out.println(tmpDirectoryName);
-		
+
 //		int x = 1;
 //		Man man = null;
 //		
@@ -20,32 +20,34 @@ public class Main {
 		Woman woman = new Woman();
 		man.setWife(woman);
 	}
-	
-	
+
 	public static int mult(int num1, int num2) {
 		if (num1 == 0) {
 			return 0;
 		}
-		
-		return num2 + mult(num1 -1, num2);
+
+		return num2 + mult(num1 - 1, num2);
 	}
-	
+
 	public static int div(int num1, int num2) {
 		if (num1 < num2) {
 			return 0;
 		}
-		
+
 		return 1 + div(num1 - num2, num2);
 	}
-	
+
 	public static int mod(int num1, int num2) {
 		if (num1 < num2) {
 			return num1;
 		}
-		
+
 		return mod(num1 - num2, num2);
 	}
-	
-	
+
+	public static int sum(int num1, int num2) {
+		int sum = num1 + num2;
+		return sum;
+	}
 
 }
